bbb5143b8f08f69e93ddbfc1377887fd74e9f3af
Eipek95 <eipek1995@gmail.com>
2024-03-26 16:20:10 +0300

    Add project files.

---
+++ Services/Catalog/MultiShop.Catalog/Controllers/ProductImagesController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.ProductImageDtos;
+using MultiShop.Catalog.Services.ProductImageServices;
+
+namespace MultiShop.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductImagesController : ControllerBase
+    {
+        private readonly IProductImageService _productImageService;
+
+        public ProductImagesController(IProductImageService productDetailService)
+        {
+            _productImageService = productDetailService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetProductImageList()
+        {
+            var values = await _productImageService.GetProductImageAllAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductImageById(string id)
+        {
+            var values = await _productImageService.GetByIdProductImageAsync(id);
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProductImage(CreateProductImageDto createProductImageDto)
+        {
+            await _productImageService.CreateProductImageAsync(createProductImageDto);
+            return Ok("Başarıyla Eklendi");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteProductImage(string id)
+        {
+            await _productImageService.DeleteProductImageAsync(id);
+            return Ok("Başarıyla Silindi");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateProductImage(UpdateProductImageDto updateProductImageDto)
+        {
+            await _productImageService.UpdateProductImageAsync(updateProductImageDto);
+            return Ok("Başarıyla Güncellendi");
+        }
+    }
+}
---
+++ Services/Catalog/MultiShop.Catalog/Extensions/StartupExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Options;
+using MultiShop.Catalog.Services.CategoryServices;
+using MultiShop.Catalog.Services.ProductDetailServices;
+using MultiShop.Catalog.Services.ProductImageServices;
+using MultiShop.Catalog.Services.ProductServices;
+using MultiShop.Catalog.Settings;
+using System.Reflection;
+
+namespace MultiShop.Catalog.Extensions
+{
+    public static class StartupExtensions
+    {
+        public static void ConfigureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<IProductImageService, ProductImageService>();
+            services.AddScoped<IProductDetailService, ProductDetailService>();
+
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            services.Configure<DatabaseSettings>(configuration.GetSection("DatabaseSettings"));
+            services.AddScoped<IDatabaseSettings>(sp =>
+            {
+                return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+            });
+        }
+    }
+}
