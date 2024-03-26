c219461031203c036c5cee81c300529394522b55
Eipek95 <eipek1995@gmail.com>
2024-03-26 23:08:14 +0300

    edit

---
+++ Services/Discount/MultiShop.Discount/Extensions/StartupExtensions.cs
@@ -0,0 +1,16 @@
+﻿using MultiShop.Discount.Context;
+using MultiShop.Discount.Services;
+
+namespace MultiShop.Discount.Extensions
+{
+    public static class StartupExtensions
+    {
+        public static void ConfigureServices(this IServiceCollection services)
+        {
+
+
+            services.AddTransient<DapperContext>();
+            services.AddTransient<IDiscountService, DiscountService>();
+        }
+    }
+}
