acf9b7be21b22bdc2d80b93636e65cb762b51296
Eipek95 <eipek1995@gmail.com>
2024-03-29 16:34:48 +0300

    edit

---
+++ Services/Cargo/MultiShop.Cargo.WebApi/Extensions/StartupExtensions.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.OpenApi.Models;
+using MultiShop.Cargo.BusinessLayer.Abstract;
+using MultiShop.Cargo.BusinessLayer.Concrete;
+using MultiShop.Cargo.DataAccesss.Abstract;
+using MultiShop.Cargo.DataAccesss.Concrete;
+using MultiShop.Cargo.DataAccesss.EntityFramework;
+using System.Reflection;
+
+namespace MultiShop.Cargo.WebApi.Extensions
+{
+    public static class StartupExtensions
+    {
+        public static void ConfigureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<CargoContext>();
+            services.AddScoped<ICargoCompanyService, CargoCompanyManager>();
+            services.AddScoped<ICargoCompanyDal, EfCargoCompanyDal>();
+
+            services.AddScoped<ICargoCustomerService, CargoCustomerManager>();
+            services.AddScoped<ICargoCustomerDal, EfCargoCustomerDal>();
+
+            services.AddScoped<ICargoDetailService, CargoDetailManager>();
+            services.AddScoped<ICargoDetailDal, EfCargoDetailDal>();
+
+            services.AddScoped<ICargoOperationService, CargoOperationManager>();
+            services.AddScoped<ICargoOperationDal, EfCargoOperationDal>();
+
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+
+            //swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "MultiShop Catalog API", Version = "v1" });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Enter your token in the text input below. Bearer prefix will be added automatically.",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+              {
+                  {
+                      new OpenApiSecurityScheme
+                      {
+                          Reference = new OpenApiReference
+                          {
+                              Type = ReferenceType.SecurityScheme,
+                              Id = "Bearer"
+                          }
+                      },
+                      new string[]{}
+                  }
+              });
+            });
+        }
+    }
+}
--- Services/Cargo/MultiShop.Cargo.WebApi/Program.cs
+++ Services/Cargo/MultiShop.Cargo.WebApi/Program.cs
@@ -1,11 +1,5 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.OpenApi.Models;
-using MultiShop.Cargo.BusinessLayer.Abstract;
-using MultiShop.Cargo.BusinessLayer.Concrete;
-using MultiShop.Cargo.DataAccesss.Abstract;
-using MultiShop.Cargo.DataAccesss.Concrete;
-using MultiShop.Cargo.DataAccesss.EntityFramework;
-using System.Reflection;
+using MultiShop.Cargo.WebApi.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,53 +14,15 @@ builder.Services.AddAuthentication(
 
     });
 
-builder.Services.AddDbContext<CargoContext>();
-builder.Services.AddScoped<ICargoCompanyService, CargoCompanyManager>();
-builder.Services.AddScoped<ICargoCompanyDal, EfCargoCompanyDal>();
 
-builder.Services.AddScoped<ICargoCustomerService, CargoCustomerManager>();
-builder.Services.AddScoped<ICargoCustomerDal, EfCargoCustomerDal>();
-
-builder.Services.AddScoped<ICargoDetailService, CargoDetailManager>();
-builder.Services.AddScoped<ICargoDetailDal, EfCargoDetailDal>();
-
-builder.Services.AddScoped<ICargoOperationService, CargoOperationManager>();
-builder.Services.AddScoped<ICargoOperationDal, EfCargoOperationDal>();
 
 builder.Services.AddControllers();
-builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+builder.Services.ConfigureServices(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSwaggerGen(c =>
-{
 
-    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "MultiShop Cargo API", Version = "v1" });
-
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Description = "JWT Authorization header using the Bearer scheme. Enter your token in the text input below. Bearer prefix will be added automatically.",
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.Http,
-        Scheme = "Bearer"
-    });
-
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-     {
-         {
-             new OpenApiSecurityScheme
-             {
-                 Reference = new OpenApiReference
-                 {
-                     Type = ReferenceType.SecurityScheme,
-                     Id = "Bearer"
-                 }
-             },
-             new string[]{}
-         }
-     });
-});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
